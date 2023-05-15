7426ad73993659fbfa6b900c4b76a96e0eda1206
张天南 <937580589@qq.com>
2023-05-15 09:16:49 +0800

    打包时生成gzip

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -6,11 +6,11 @@ const baseConfig = require('./webpack.base.js');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const globAll = require('glob-all');
-const {PurgeCSSPlugin} = require('purgecss-webpack-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(baseConfig, {
     mode: 'production',// 生产环境
@@ -50,6 +50,14 @@ module.exports = merge(baseConfig, {
                 }
             }
         }),
+        new CompressionPlugin({
+            test: /.(js|css)$/, // 只生成css,js压缩文件
+            filename: '[path][base].gz', // 文件命名
+            algorithm: 'gzip', // 压缩格式,默认是gzip
+            test: /.(js|css)$/, // 只生成css,js压缩文件
+            threshold: 10240, // 只有大小大于该值的资源会被处理。默认值是 10k
+            minRatio: 0.8 // 压缩率,默认值是 0.8
+        })
     ],
     optimization: {
         minimizer: [
